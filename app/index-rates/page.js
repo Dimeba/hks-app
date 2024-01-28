f5002ce54fd3376ca7ede19ec1c7fcc5aaa8c7ad
Filip Stojanovic <philsty92@gmail.com>
2024-01-28 07:38:10 -0500

    Added description to Index Rates.

--- app/index-rates/page.js
+++ app/index-rates/page.js
@@ -1,13 +1,27 @@
+// contentful
+import { createClient } from 'contentful'
+
 // components
 import Container from '@/components/global/Container'
 import SectionTitle from '@/components/global/SectionTitle'
 
 export default async function IndexRates() {
+	const client = createClient({
+		space: process.env.space,
+		accessToken: process.env.accessToken
+	})
+
+	const rates = await client.getEntries({
+		content_type: 'indexRatesSection'
+	})
 	return (
 		<main>
 			<section className='section-gray'>
 				<Container newClass='rates'>
-					<SectionTitle text='Index Rates' />
+					<SectionTitle
+						text='Index Rates'
+						subtitle={rates.items[0].fields.description}
+					/>
 					<iframe
 						id='rates'
 						src='https://www.thefinancials.com/Widget.aspx?pid=HKS&wid=0344406736&style=table_tall_graph'
