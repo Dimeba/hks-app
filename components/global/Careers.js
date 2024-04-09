0c76ccbe490fee4b1c9ff1138348cb165d08980d
Filip Stojanovic <philsty92@gmail.com>
2024-04-09 05:42:00 -0400

    Added careers banner.

--- components/global/Careers.js
+++ components/global/Careers.js
@@ -8,37 +8,56 @@ import Link from 'next/link'
 import { BiPlus } from 'react-icons/bi'
 import { BiMinus } from 'react-icons/bi'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import ImageContainer from './ImageContainer'
 
 const Careers = ({ careers }) => {
 	return (
-		<section id='careers'>
-			<Container>
-				<SectionTitle text={careers.fields.title} />
+		<>
+			<section id='careers'>
+				<Container>
+					<SectionTitle text={careers.fields.title} />
 
-				<div className={styles.careersContainer}>
-					<div className={styles.careers}>
-						{careers.fields.careers.map(item => (
-							<Link
-								key={item.sys.id}
-								href={'https:' + item.fields.file.fields.file.url}
-								target='_blank'
-								aria-label='Link to document'
-							>
-								<div>
-									<BiPlus color='#e63122' size='24px' />
+					<div className={styles.careersContainer}>
+						<div className={styles.careers}>
+							{careers.fields.careers.map(item => (
+								<Link
+									key={item.sys.id}
+									href={'https:' + item.fields.file.fields.file.url}
+									target='_blank'
+									aria-label='Link to document'
+								>
+									<div>
+										<BiPlus color='#e63122' size='24px' />
 
-									<h4>{item.fields.title}</h4>
-								</div>
-							</Link>
-						))}
+										<h4>{item.fields.title}</h4>
+									</div>
+								</Link>
+							))}
+						</div>
+
+						<div className={styles.description}>
+							{documentToReactComponents(careers.fields.description)}
+						</div>
 					</div>
+				</Container>
+			</section>
 
-					<div className={styles.description}>
-						{documentToReactComponents(careers.fields.description)}
-					</div>
-				</div>
-			</Container>
-		</section>
+			{careers.fields.desktopBanner && (
+				<ImageContainer
+					src={'https:' + careers.fields.desktopBanner.fields.file.url}
+					className={styles.desktopBanner}
+					alt='Work for us Banner.'
+				/>
+			)}
+
+			{careers.fields.mobileBanner && (
+				<ImageContainer
+					src={'https:' + careers.fields.mobileBanner.fields.file.url}
+					className={styles.mobileBanner}
+					alt='Work for us Banner.'
+				/>
+			)}
+		</>
 	)
 }
 
